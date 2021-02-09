e6b282255b05393439feaf8524f8a402e5bcce9e
przemodeveloper <przemyslaw.gorecki93@gmail.com>
2021-02-09 11:11:39 +0100

    Add initial navbar

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Navigation.scss';
+
+const Navigation = () => {
+    return(
+        <nav>
+            <ul className="navbar">
+                <li>Sign out</li>
+                <li>Info</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
