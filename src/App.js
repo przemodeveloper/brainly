0e3666916da97b33fa8208f144fd4519f69244c2
przemodeveloper <przemyslaw.gorecki93@gmail.com>
2021-02-11 14:30:17 +0100

    Add styles

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import './App.scss';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageForm from './Components/ImageForm/ImageForm';
+import Ranking from './Components/Ranking/Ranking';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  "particles": {
+    "number": {
+      "value":80,
+      "density": {
+        "enable": true,
+        "value_area":800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
+        <Ranking />
         <ImageForm />
         {/* <FaceRecognition /> */}
       </div>
---
+++ src/Components/Ranking/Ranking.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Ranking.scss';
+
+const Ranking = () => {
+    return(
+        <div>
+            <div>
+                <h1>Przemek, your current rank is...</h1>
+            </div>
+            <div>
+                <h2>#5</h2>
+            </div>
+        </div>
+    )
+};
+
+export default Ranking;
